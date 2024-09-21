2a2b10ee5185ddc95812f83feb833d903dc7b218
eminamarmarac <mac@Macs-MacBook-Pro.local>
2024-09-21 17:59:43 +0200

    Initial commit

---
+++ src/main/java/com/emina/file_system_explorer/FileSystemExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.emina.file_system_explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSystemExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emina/file_system_explorer/FileSystemExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emina.file_system_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSystemExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
